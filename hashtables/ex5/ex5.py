82c03f1c15156a52388ba5c5b7d28c112a471aa5
Derek <legato_5@yahoo.com>
2020-09-13 20:37:11 -0700

    4 out of 5 problems completed

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -8,17 +8,19 @@ def finder(files, queries):
     """
     # Your code here
     file_hash = {}
+    query_hash = {}
     result = []
     i = 0
     while i < len(files):
-        file_hash[files[i]] = files[i]
+        file_hash[files[i]] = files[i].split("/")[-1]
         i+=1
     a = 0
     while a < len(queries):
-        if a < len(files):
-            if queries[a] in file_hash[files[a]]:
-                result.append(file_hash[files[a]])
+        query_hash[queries[a]] = queries[a]
         a+=1
+    for item in file_hash:
+        if file_hash[item] in query_hash:
+            result.append(item)
     return result
 
 
